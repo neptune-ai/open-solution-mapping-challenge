048339c9457f0db57461bfd77c9d81a3a2c92666
apyskir <andrzej.pyskir@gmail.com>
2018-04-20 12:41:12 +0200

    fix postpro

--- postprocessing.py
+++ postprocessing.py
@@ -20,7 +20,7 @@ class BuildingLabeler(BaseTransformer):
     def transform(self, images):
         labeled_images = []
         for i, image in enumerate(images):
-            labeled_image = label(image)
+            labeled_image = label_multichannel_image(image)
             labeled_images.append(labeled_image)
 
         return {'labeled_images': labeled_images}
@@ -29,3 +29,10 @@ class BuildingLabeler(BaseTransformer):
 def label(mask):
     labeled, nr_true = ndi.label(mask)
     return labeled
+
+def label_multichannel_image(mask):
+    labeled_channels = []
+    for channel in mask:
+        labeled_channels.append(label(channel))
+    labeled_image = np.stack(labeled_channels)
+    return labeled_image
