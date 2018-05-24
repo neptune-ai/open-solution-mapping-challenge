b3142356386bdf77003eb72abc4df09838764479
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-05-24 15:18:02 +0200

    added initial version

---
+++ evaluate_checkpoint.py
@@ -0,0 +1,62 @@
+import yaml
+import subprocess
+import os
+
+import click
+
+MISSING_TRANSFORMERS = ['prediction_crop',
+                        'prediction_renamed',
+                        'mask_resize',
+                        'category_mapper',
+                        'mask_erosion',
+                        'labeler',
+                        'mask_dilation',
+                        'score_builder',
+                        'output']
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option('-e', '--experiment_dir', help='experiment that you want to run evaluation on', required=True)
+@click.option('-t', '--temp_inference_dir', help='temporary directory', required=True)
+def run_inference(temp_inference_dir, experiment_dir):
+    transformer_dir = os.path.join(temp_inference_dir, 'transformers')
+    checkpoints_dir = os.path.join(temp_inference_dir, 'checkpoints')
+
+    cmd = 'cp -rf {} {}'.format(experiment_dir, temp_inference_dir)
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'cp {}/unet/best.torch {}/unet'.format(checkpoints_dir, transformer_dir)
+    subprocess.call(cmd, shell=True)
+
+    for missing_transformer in MISSING_TRANSFORMERS:
+        cmd = 'touch {}/{}'.format(transformer_dir, missing_transformer)
+        subprocess.call(cmd, shell=True)
+
+    cmd = 'cp neptune.yaml temporary_neptune.yaml'.format(checkpoints_dir, transformer_dir)
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'cp neptune.yaml temporary_neptune.yaml'.format(checkpoints_dir, transformer_dir)
+    subprocess.call(cmd, shell=True)
+
+    with open("temporary_neptune.yaml", 'rw') as f:
+        doc = yaml.load(f)
+        doc['experiment_dir'] = temp_inference_dir
+        yaml.dump(f)
+
+    cmd = 'neptune run --config temporary_neptune.yaml -- evaluate -p unet_weighted_padded'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'rm temporary_neptune.yaml'
+    subprocess.call(cmd, shell=True)
+
+    cmd = 'rm -rf {}'.format(temp_inference_dir)
+    subprocess.call(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
